9127329df0c1dbf27bf6b97cadfec3337eaedfb8
Carlos <utm22030587@utma.edu.mx>
2023-06-15 20:34:11 -0600

    feat: Se añade la clase Superhero con sus métodos y atributos

--- pythonGame.py
+++ pythonGame.py
@@ -1,3 +1,4 @@
+import random                       
 
 class SuperheroType:
     def __init__(self, name, strengths=None, weaknesses=None):
@@ -14,6 +15,72 @@ class SuperheroType:
     def is_weak_against(self, other):
 
         return other.name in self.strengths
+
+
+class Superhero:
+    def __init__(self, name, level, types):
+
+        self.name = name
+        self.level = level
+        self.types = types
+        self.max_health = level * 10
+        self.current_health = self.max_health
+        self.is_dead = False
+
+    def __repr__(self):
+
+        type_names = "/".join([t.name for t in self.types])
+        return f"{self.name} ({type_names} - Level {self.level})"
+
+    def lose_health(self, damage):
+
+        self.current_health = max(0, self.current_health - damage)
+        if self.current_health == 0:
+            self.dead()
+
+    def gain_health(self):
+
+        self.current_health = self.max_health
+
+    def dead(self):
+        
+        self.is_dead = True
+
+    def revive(self):
+       
+        self.is_dead = False
+        self.current_health = self.max_health
+
+    def get_effectiveness(self, other_type):
+       
+        effectiveness = 1.0
+        
+        for type in self.types:
+            for strength in type.strengths:
+                if strength == other_type.name:
+
+                    effectiveness *= 1.5
+                    
+            for weakness in type.weaknesses:
+                if weakness == other_type.name:
+
+                    effectiveness *= 0.8
+
+        return effectiveness
+
+    def attack(self, other):
+       
+        effectiveness = self.get_effectiveness(other.types[0])
+        if len(other.types) > 1:
+            effectiveness *= self.get_effectiveness(other.types[1])
+
+        num1=int(effectiveness*self.level)
+        num2=int(effectiveness*self.level/2)
+
+        damage=random.randint(num2, num1)
+        
+        print(f"{self} attacks {other} and deals {damage} damage!")
+        other.lose_health(damage)
 
 #The type of superhero is defined, its fortress and debility
 
